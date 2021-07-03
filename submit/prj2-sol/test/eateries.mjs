1737dee8e05c5416b3e65bf5b9f6484346915caf
SUBRAHMANYA U <su1@binghamton.edu>
2021-07-02 21:01:30 -0400

    test case

--- submit/prj2-sol/test/eateries.mjs
+++ submit/prj2-sol/test/eateries.mjs
@@ -7,7 +7,7 @@ import chai from 'chai';
 const { assert } = chai;
 
 const COURSE_DIR = `${process.env.HOME}/cs544`;
-const DATA_PATH = `${COURSE_DIR}/data/chow-down1.json`;
+const DATA_PATH = "/Users/subrahmanya/Documents/GitHub/Web-Projects-CS544/submit/prj2-sol/chow-down1.json";
 const DATA = readJson(DATA_PATH);
 
 describe('eateries DAO', function() {
@@ -16,6 +16,7 @@ describe('eateries DAO', function() {
 
   beforeEach(async () => {
     dao = await setupDao();
+    console.log(dao);
     await dao.loadEateries(DATA);
   });
 
@@ -25,16 +26,17 @@ describe('eateries DAO', function() {
 
   it ('must find Chinese cuisine', async function () {
     const results = await dao.locateEateries('Chinese');
+    console.log("result "+JSON.stringify(results))
     assert.equal(results.length, 5);
   });
 
-  it ('must find all Chinese cuisine', async function () {
+ it ('must find all Chinese cuisine', async function () {
     const results = await dao.locateEateries('Chinese', params.bingLoc,
 					     0, 100 );
     assert.equal(results.length, 19);
   });
 
-  it ('must find cuisine sorted by dist', async function () {
+ it ('must find cuisine sorted by dist', async function () {
     const results = await dao.locateEateries('indian');
     assert.isAbove(results?.length, 0);
     assert(results.every((r, i, res) => i === 0 || r.dist >= res[i - 1].dist));
@@ -62,10 +64,10 @@ describe('eateries DAO', function() {
     const id = results[0].id;
     const eatery = await dao.getEatery(id);
     assert.equal(eatery.id, id);
-    assert(eatery.menu);
+    assert(JSON.stringify(eatery.menu));
   });
 
-  it ('must verify eatery has requested cuisine', async function () {
+ it ('must verify eatery has requested cuisine', async function () {
     const CUISINE = 'chinese';
     const results = await dao.locateEateries(CUISINE);
     for (const r of results) {
@@ -81,6 +83,60 @@ describe('eateries DAO', function() {
     assert.isAbove(eatery.errors?.length, 0);
     assert.equal(eatery.errors[0].code, 'NOT_FOUND');
   });
+
+  it ('must insert new Order', async function () {
+    const id = 1;
+    const insertRes = await dao.newOrder(id) 
+    assert.equal(id, insertRes.eateryId);
+  });
+
+  it ('can not create new Order', async function () {
+    const id = 0;
+    const insertRes = await dao.newOrder(id);
+    assert.equal(insertRes.errors[0].code, "DB");
+  });
+
+  it ('must get Orderinformation based on OrderNumber', async function () {
+    const id = "2_62";
+    const getRes = await dao.getOrder(id) 
+   // assert(getRes)
+    assert.equal(id, getRes._id);
+  });
+
+  it ('must return NOT-FOUND error if given OrderNumber not Present', async function () {
+    const id = "0_56";
+    const getRes = await dao.getOrder(id);
+    assert.equal(getRes.errors[0].code, "NOT_FOUND");
+  });
+
+  it ('must removeOrder based on OrderNumber', async function () {
+    const id = "1_67";
+    const getRes = await dao.removeOrder(id) 
+    assert.equal(1, getRes.deletedCount);
+  });
+
+  it ('must return NOT-FOUND error if given OrderNumber not Present to removeOrder ', async function () {
+    const id = "0_56";
+    const getRes = await dao.removeOrder(id);
+    assert.equal(getRes.errors[0].code, "NOT_FOUND");
+  });
+
+  it ('must editOrder based on OrderNumber', async function () {
+    const orderId = "2_62";
+    const itemId = "2_44";
+    const nChanges = 5;
+    const getRes = await dao.editOrder(orderId, itemId, nChanges) 
+    assert.equal(orderId, getRes._id);
+  });
+
+  it ('must return NOT-FOUND error if given OrderNumber not Present to removeOrder ', async function () {
+    const id = "0_56";
+    const getRes = await dao.removeOrder(id);
+    assert.equal(getRes.errors[0].code, "NOT_FOUND");
+  }); 
+
+
+
 
 });	
 
