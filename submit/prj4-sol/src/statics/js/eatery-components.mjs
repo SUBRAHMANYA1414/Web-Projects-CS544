ca3c4cd9977283ec67a6780e817cf40e07c348e7
SUBRAHMANYA U <su1@binghamton.edu>
2021-07-31 21:06:09 -0400

    check eatery details

--- submit/prj4-sol/src/statics/js/eatery-components.mjs
+++ submit/prj4-sol/src/statics/js/eatery-components.mjs
@@ -17,20 +17,20 @@ import { newElement, geoLoc } from './util.mjs';
     its value.
 
     cuisine: when changed, the component should make a web-service call
-    to search for eateries for that cuisine with location set to the 
+    to search for eateries for that cuisine with location set to the
     browser's location.  Then it should set it's content corresponding
     to the pseudo-HTML shown below (dynamic data is shown within ${...} and
     wsData is the data returned from the web-service call):
 
       <ul class="eatery-results">
-	<!-- repeat for each eatery in wsData.eateries -->
-	<li>
-	  <span class="eatery-name">${eatery.name}</span>
-	  <span>${eatery.dist} miles</span>
-	  <a href=${links:self.href}>
-	    <button>Select</button>
-	  </a>
-	</li>
+<!-- repeat for each eatery in wsData.eateries -->
+<li>
+ <span class="eatery-name">${eatery.name}</span>
+ <span>${eatery.dist} miles</span>
+ <a href=${links:self.href}>
+   <button>Select</button>
+ </a>
+</li>
       </ul>
 
     The handler for the Select button should be set up to set
@@ -39,14 +39,14 @@ import { newElement, geoLoc } from './util.mjs';
     This should be followed by up-to two scrolling links:
 
       <div class="scroll">
-	<!-- only when ${wsData.links:prev} -->
-	<a rel="prev" href="${wsData.links:prev.href}">
-	  <button>&lt;</button>
-	</a>
-	<!-- only when ${wsData.links:next} -->
-	<a rel="next" href="${wsData.links:next.href}">
-	  <button>&gt;</button>
-	</a>
+<!-- only when ${wsData.links:prev} -->
+<a rel="prev" href="${wsData.links:prev.href}">
+ <button>&lt;</button>
+</a>
+<!-- only when ${wsData.links:next} -->
+<a rel="next" href="${wsData.links:next.href}">
+ <button>&gt;</button>
+</a>
       </div>
 
     When the above scrolling links are clicked, the results should
@@ -56,28 +56,111 @@ import { newElement, geoLoc } from './util.mjs';
 class EateryResults extends HTMLElement {
 
 
-  static get observedAttributes() { return [ 'ws-url', 'cuisine', ]; }
+    static get observedAttributes() { return [ 'ws-url', 'cuisine', ]; }
 
-  async attributeChangedCallback(name, oldValue, newValue) {
-    //TODO
-    super();
-    this.addEventListener('login', this.login);
-    this.attachShadow({mode: 'open'});
-    this.tick = this.tick.bind(this);
-  }
+    async attributeChangedCallback(name, oldValue, newValue) {
+        try {
 
-  //TODO auxiliary methods
-  connectedCallback() {
-    this.shadowRoot.innerHTML = 'app';
-  }
+
+            let AcualUrl;
+            const location = await geoLoc()
+            const baseurl = this.getAttribute('ws-url')
+            const url = new URL(baseurl);
+            url.pathname = 'eateries' + '/' + location.lat
+                + "," + location.lng
+            if(!oldValue) {
+                AcualUrl = url + "?cuisine" + "=" + newValue
+            }else {
+                if (name === "a") {
+                    AcualUrl = oldValue
+                }
+                else{
+                    AcualUrl = url + "?cuisine" + "=" + newValue
+                }
+            }
+            let wsData=await fetchData(AcualUrl)
+            // let a=wsDataFn(wsData,baseurl)
+            let hdr,hdr2
+            hdr=newElement('ul', {class: 'eatery-results'},);
+
+            for (let i of wsData.eateries) {
+                const spanattribute=newElement('span', {class: 'eatery-name'}, i.name);
+                const spanattribute1=newElement('span', {}, i.dist + " " + "miles");
+                const buttonSelect = newElement('button', {}, 'Select');
+                buttonSelect.addEventListener('click', async ev => {
+                    ev.currentTarget
+                    ev.preventDefault()
+                    await this.attributeChangedCallback('a', ev.currentTarget.parentNode.href, 'b')
+
+                    document.querySelector('eatery-details').setAttribute('eatery-url', `${baseurl}/eateries/${i.id}`)
+
+                    // this.);
+                });
+                const aattribute=newElement('a', {class:'select-eatery', href: getHref(wsData.links, 'self')},buttonSelect );
+
+                hdr2=newElement('li', {},);
+                hdr2.append(spanattribute)
+                hdr2.append(spanattribute1)
+                hdr2.append(aattribute)
+                hdr.append(hdr2)
+
+            }
+            const hdr6=newElement('div', {class: 'scroll'})
+
+            if(getHref(wsData.links,'prev')!==undefined)
+            {
+                const buttonLt = newElement('button', {}, '<');
+                buttonLt.addEventListener('click', async ev => {
+                    // ev.currentTarget.parentNode.href
+                    ev.preventDefault()
+
+                    await this.attributeChangedCallback('a', ev.currentTarget.parentNode.href, 'b')
+
+
+                });
+
+                const hdr4 = newElement('a', {rel: 'prev', href: getHref(wsData.links, 'prev')}, buttonLt)
+                hdr6.append(hdr4)
+
+            }
+            if(getHref(wsData.links,'next')!==undefined) {
+                const buttonGt = newElement('button', {}, `>`);
+                buttonGt.addEventListener('click', async ev => {
+                    ev.preventDefault()
+                    await this.attributeChangedCallback('a', ev.currentTarget.parentNode.href,newValue )
+                });
+
+                const hdr5 = newElement('a', {rel: 'next', href: getHref(wsData.links, 'next')}, buttonGt)
+
+                hdr6.append(hdr5)
+            }
+
+
+
+            this.innerHTML=""
+            this.append(hdr)
+
+            this.append(hdr6)
+
+
+
+
+        }
+        catch (err) {
+            return new AppErrors().add(err);
+        }
+    }
+
+    //TODO auxiliary methods
 }
 
 //register custom-element as eatery-results
 customElements.define('eatery-results', EateryResults);
 
 
+
 /*
-  A component which shows the details of an eatery.  
+  A component which shows the details of an eatery.
 
   When created, it is set up with a buyFn *property* which should be
   called with an eatery-id and item-id to order a single unit of the
@@ -96,8 +179,8 @@ customElements.define('eatery-results', EateryResults);
 
       <h2 class="eatery-name">${wsData.name} Menu</h2>
       <ul class="eatery-categories">
-	<!-- repeat for each category in wsData.menuCategories -->
-	<button class="menu-category">${category}</button>
+<!-- repeat for each category in wsData.menuCategories -->
+<button class="menu-category">${category}</button>
       </ul>
       <!-- will be populated with items for category when clicked above -->
       <div id="category-details"></div>
@@ -107,54 +190,134 @@ customElements.define('eatery-results', EateryResults);
 
       <h2>${category}</h2>
       <ul class="category-items">
-	<!-- repeat for each item in wsData.flatMenu[wsData.menu[category]] -->
-	<li>
-	  <span class="item-name">${item.name}</span>
-	  <span class="item-price">${item.price}</span>
-	  <span class="item-details">${item.details}</span>
-	  <button class="item-buy">Buy</button>
-	</li>
+<!-- repeat for each item in wsData.flatMenu[wsData.menu[category]] -->
+<li>
+ <span class="item-name">${item.name}</span>
+ <span class="item-price">${item.price}</span>
+ <span class="item-details">${item.details}</span>
+ <button class="item-buy">Buy</button>
+</li>
       </ul>
 
   The handler for the Buy button should be set up to call
   buyFn(eatery.id, item.id).
 
 */
+
 class EateryDetails extends HTMLElement {
 
-  static get observedAttributes() { return [ 'eatery-url', ]; }
-  
-  async attributeChangedCallback(name, oldValue, newValue) {
-    //TODO
-    async function go() {
-      const wsUrl = getWsUrl();
-    
-      const eateryResults = document.querySelector('eatery-results');
-      eateryResults.setAttribute('ws-url', wsUrl);
-      document.querySelector('eatery-details').buyFn = buy;
-      
-  }
+    static get observedAttributes() { return [ 'eatery-url', ]; }
 
-  //TODO auxiliary methods
-  const select = document.querySelector('#cuisine');
-  select.addEventListener('change', ev => {
-    const cuisine = ev.target.value;
-    if (cuisine) eateryResults.setAttribute('cuisine', cuisine);
-  });
-}
+    async attributeChangedCallback(name, oldValue, newValue) {
+        try {
+
+            const wsData=await fetchData(this.getAttribute('eatery-url'))
+            let hdr5,hdr6,hdr7,button,ulattribute1,categoryDetails
+            const name = `${wsData.name} Menu`;
+            const hdr = newElement('h2', { class: 'eatery-name' }, name);
+            const ulattribute=newElement('ul', { class: 'eatery-categories' });
+            for (var i = 0; i < wsData.menuCategories.length; i++) {
+                const category1 = wsData.menuCategories[i]
+                this.innerHTML="";
+
+                button = newElement('button', {class: 'menu-category'}, category1);
+                button.addEventListener('click', ev => {
+                    ev.preventDefault();
+                    const div = document.querySelector('#category-details');
+                    div.innerHTML = ""
+                    // categoryDetails=newElement('div', { id: 'category-details' },);
+                    hdr6=newElement('h2',{} ,category1 );
+                    // if(document.getElementById('category-items')!==null ) {
+                    //     document.getElementById('category-items').innerHTML = ""
+                    // }
+                    ulattribute1=newElement('ul',{class:'category-items'},)
+
+                    for(i of wsData.menu[category1]) {
+
+                        hdr5=newElement('li', {},);
+                        const spanattribute=newElement('span', {class: 'item-name'},wsData.flatMenu[i].name);
+                        const spanattribute1=newElement('span', {class: 'item-price'},wsData.flatMenu[i].price);
+                        const spanattribute2=newElement('span', {class: 'item-details'},wsData.flatMenu[i].details);
+
+                        const buttonItemBuy = newElement('button', { class: 'item-buy' },'Buy');
+                        buttonItemBuy.addEventListener('click', ev => {
+                            this.buyFn(wsData.id,wsData.flatMenu[i].id)
+                            ev.preventDefault();
+                        });
+
+                        hdr5.append(spanattribute)
+                        hdr5.append(spanattribute1)
+                        hdr5.append(spanattribute2)
+                        hdr5.append(buttonItemBuy)
+                        ulattribute1.append(hdr5)
+
+                    }
+
+
+                    div.append(hdr6)
+                    div.append(ulattribute1)
+                    div.scrollIntoView();
+
+
+                    // if(document.getElementById('category-details')!==null) {
+                    //     document.getElementById('category-details').innerHTML=""
+                    // }
+                   // this.append(categoryDetails);
+
+
+                });
+
+                ulattribute.append(button)
+
+            }
+
+            this.append(hdr)
+            this.append(ulattribute);
+            categoryDetails=newElement('div', { id: 'category-details' },);
+
+            this.append(categoryDetails);
+
+
+
+
+        }
+        catch (err) {
+            return new AppErrors().add(err);
+        }
+        //TODO
+    }
+
+
+    //TODO auxiliary methods
+
 }
 
 //register custom-element as eatery-details
 customElements.define('eatery-details', EateryDetails);
 
+async function fetchData(url = '', data = {}) {
+
+    const response = await fetch(url, {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+    });
+    return response.json();
+}
+
+
+
 /** Given a list of links and a rel value, return the href for the
  *  link in links having the specified value.
  */
 function getHref(links, rel) {
-  return links.find(link => link.rel === rel)?.href;
+    return links.find(link => link.rel === rel)?.href;
 }
 
 //TODO auxiliary functions
-function buy(eateryId, itemId) {
-  alert(`added 1 unit of ${itemId} from eatery ${eateryId} to order`);
-}
